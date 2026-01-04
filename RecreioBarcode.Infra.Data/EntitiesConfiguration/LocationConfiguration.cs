d2390b28bcbc512a416c67f0b9c73329e8295442
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-04 16:09:47 -0300

    Criado arquivo para configurar as tabelas pelo EntityFramework.

---
+++ RecreioBarcode.Infra.Data/EntitiesConfiguration/LocationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreioBarcode.Infra.Data.EntitiesConfiguration
+{
+    internal class LocationConfiguration
+    {
+    }
+}
---
+++ RecreioBarcode.Infra.Data/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreioBarcode.Infra.Data.EntitiesConfiguration
+{
+    class UserConfiguration
+    {
+    }
+}
