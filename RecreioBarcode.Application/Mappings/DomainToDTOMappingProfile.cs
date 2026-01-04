5805e14bf211551cec8f907e51c8f06a590b5bca
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-04 16:11:53 -0300

    Criado e configurado arquivo de mapping para usar o AutoMapper.

---
+++ RecreioBarcode.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RecreioBarcode.Application.DTOs;
+using RecreioBarcode.Domain.Entities;
+
+namespace RecreioBarcode.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Inventory,InventoryDTO>().ReverseMap();
+            CreateMap<InventoryItemOut,InventoryItemOutDTO>().ReverseMap();
+            CreateMap<InventoryLine,InventoryLineDTO>().ReverseMap();
+            CreateMap<InventoryLocation,InventoryLocationDTO>().ReverseMap();
+            CreateMap<Location,LocationDTO>().ReverseMap();
+            CreateMap<User,UserDTO>().ReverseMap();
+        }
+    }
+}
