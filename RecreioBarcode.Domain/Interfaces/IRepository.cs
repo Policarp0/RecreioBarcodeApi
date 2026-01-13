92e7af12dce241867b4325dfc047e2f6a118f75c
Policarp0 <T.policarpo99@gmail.com>
2026-01-13 17:47:49 -0300

    Alterada a interface de repositório para a versão final.

--- RecreioBarcode.Domain/Interfaces/IRepository.cs
+++ RecreioBarcode.Domain/Interfaces/IRepository.cs
@@ -4,8 +4,11 @@ namespace RecreioBarcode.Domain.Interfaces
 {
     public interface IRepository<T>
     {
-        Task <IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
-        Task<T?> Get(Expression<Func<T,bool>> predicate);
+        Task<T?> GetByIdAsync(int id);
+        Task<T?> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task <IEnumerable<T>> GetAllAsync();
+        Task <IEnumerable<T>> GetAllWhereAsync(Expression<Func<T, bool>> predicate);
+        
         T Create(T entity);
         T Update(T entity);
         T Delete(T entity);
