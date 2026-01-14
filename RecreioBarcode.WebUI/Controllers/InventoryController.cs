c755d38b29f6319f5810dba909fd05bba612c1f5
Policarp0 <T.policarpo99@gmail.com>
2026-01-14 17:05:23 -0300

    Adicionado método para criar inventário a partir de um arquivo csv.

--- RecreioBarcode.WebUI/Controllers/InventoryController.cs
+++ RecreioBarcode.WebUI/Controllers/InventoryController.cs
@@ -6,10 +6,28 @@ namespace RecreioBarcode.WebUI.Controllers;
 public class InventoryController(IInventoryService inventoryService) : Controller
 {
     private readonly IInventoryService _inventoryService = inventoryService;
+
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         var Inventories = await _inventoryService.GetAllAsync();
         return View(Inventories);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateFromCsv([FromForm] string name, IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return Redirect("/index");
+
+        var allowedExtensions = new[] { ".csv", ".txt" };
+        var extension = Path.GetExtension(file.FileName)?.ToLowerInvariant();
+        if (string.IsNullOrWhiteSpace(extension) || !allowedExtensions.Contains(extension))
+            return BadRequest("Tipo de arquivo inválido.");
+
+        await using var stream = file.OpenReadStream();
+        var result = await _inventoryService.CreateFromCsvAsync(name,stream);
+
+        return View();
     }
 
 }
