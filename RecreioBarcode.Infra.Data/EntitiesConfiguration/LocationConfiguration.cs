4313f0a1a83b55e00caa69bcdeb670fcae5524d4
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-17 13:02:01 -0300

    Removido o método HasData de Location.

--- RecreioBarcode.Infra.Data/EntitiesConfiguration/LocationConfiguration.cs
+++ RecreioBarcode.Infra.Data/EntitiesConfiguration/LocationConfiguration.cs
@@ -9,13 +9,6 @@ namespace RecreioBarcode.Infra.Data.EntitiesConfiguration
         public void Configure(EntityTypeBuilder<Location> builder)
         {
             builder.HasKey(x => x.Id);
-
-            builder.HasData(
-                new Location(1,'A',1,1,'A',1),
-                new Location(2,'A',2,99,'B',1),
-                new Location(3,'B',1,99,'C',1),
-                new Location(4,'B',2,99,'D',2),
-                new Location(5,'Z',99,99,'Z',99));
         }
     }
 }
