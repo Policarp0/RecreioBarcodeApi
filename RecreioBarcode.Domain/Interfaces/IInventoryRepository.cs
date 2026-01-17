af90bfb39f1b44c48ea7f0047980a42f1adeef76
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-17 13:00:30 -0300

    Repository alterado para específico para atender apenas aos Aggregate Roots.

---
+++ RecreioBarcode.Domain/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using RecreioBarcode.Domain.Entities;
+
+namespace RecreioBarcode.Domain.Interfaces;
+
+public interface IInventoryRepository
+{
+    Task<Inventory?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<Inventory?> GetSummaryByIdAsync(int id, CancellationToken ct = default);
+    Task AddAsync(Inventory inventory, CancellationToken ct = default);
+}
---
+++ RecreioBarcode.Domain/Interfaces/ILocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecreioBarcode.Domain.Entities;
+
+namespace RecreioBarcode.Domain.Interfaces;
+
+public interface ILocationRepository
+{
+    Task<Location?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task AddAsync(Location location, CancellationToken ct = default);
+}
