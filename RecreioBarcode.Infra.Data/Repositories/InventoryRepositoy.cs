8ce238cc4f82abe76f8729cce4e545e80154ba45
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-17 13:00:45 -0300

    Repository alterado para específico para atender apenas aos Aggregate Roots.

---
+++ RecreioBarcode.Infra.Data/Repositories/InventoryRepositoy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecreioBarcode.Domain.Entities;
+using RecreioBarcode.Domain.Interfaces;
+using RecreioBarcode.Infra.Data.Context;
+
+namespace RecreioBarcode.Infra.Data.Repositories;
+
+public class InventoryRepository(ApplicationContext context) : IInventoryRepository
+{
+    private readonly ApplicationContext _context = context;
+    
+    public async Task<Inventory?> GetByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Inventories
+            .Include(i => i.InventoryLocations)
+                .ThenInclude(i => i.InventoryLines)
+            .Include(i => i.InventoryItemsOut)
+            .FirstOrDefaultAsync(i => i.Id == id, ct);
+    }
+    public async Task<Inventory?> GetSummaryByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Inventories.FindAsync([id, ct], cancellationToken: ct);
+    }
+    public async Task AddAsync(Inventory inventory, CancellationToken ct = default)
+    {
+        await _context.Inventories.AddAsync(inventory, ct);
+    }
+
+}
---
+++ RecreioBarcode.Infra.Data/Repositories/LocationRepository.cs
@@ -0,0 +1,19 @@
+﻿using RecreioBarcode.Domain.Entities;
+using RecreioBarcode.Domain.Interfaces;
+using RecreioBarcode.Infra.Data.Context;
+
+namespace RecreioBarcode.Infra.Data.Repositories;
+
+public class LocationRepository(ApplicationContext context) : ILocationRepository
+{
+    private readonly ApplicationContext _context = context;
+
+    public async Task<Location?> GetByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Locations.FindAsync([id, ct], cancellationToken: ct);
+    }
+    public async Task AddAsync(Location location, CancellationToken ct = default)
+    {
+        await _context.Locations.AddAsync(location, ct);
+    }
+}
