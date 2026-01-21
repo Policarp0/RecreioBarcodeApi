cb5f87749c90a1aa0f183a5b4b07cfe365fcc862
Policarpo0 <T.Policarpo99@gmail.com>
2026-01-21 08:23:09 -0300

    TESTE

---
+++ RecreioBarcode.Infra.Data/Migrations/20260121112242_AAAAA.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecreioBarcode.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AAAAA : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalInventoriedLines",
+                table: "InventoryLocations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalLines",
+                table: "InventoryLocations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "TotalInventoriedLines",
+                table: "InventoryLocations");
+
+            migrationBuilder.DropColumn(
+                name: "TotalLines",
+                table: "InventoryLocations");
+        }
+    }
+}
